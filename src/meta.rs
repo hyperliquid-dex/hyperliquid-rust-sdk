b6fbf9c1d0173cc713b73a41634dfceb287ad1c5
chameleon <chameleon@admins-MacBook-Pro.local>
2023-08-03 16:24:56 -0400

    add clone to meta

--- src/meta.rs
+++ src/meta.rs
@@ -1,11 +1,11 @@
 use serde::Deserialize;
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 pub struct Meta {
     pub universe: Vec<AssetMeta>,
 }
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct AssetMeta {
     pub name: String,
