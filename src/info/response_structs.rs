1f5fe9c3fdf0fde665767ea79b8f885e636f9c70
sr-fuentes <29989568+sr-fuentes@users.noreply.github.com>
2023-12-04 09:49:34 -0500

    Update response_structs.rs OpenOrder oid (#5)

--- src/info/response_structs.rs
+++ src/info/response_structs.rs
@@ -15,7 +15,7 @@ pub struct UserStateResponse {
 pub struct OpenOrdersResponse {
     pub coin: String,
     pub limit_px: String,
-    pub oid: i32,
+    pub oid: u64,
     pub side: String,
     pub sz: String,
     pub timestamp: u64,
