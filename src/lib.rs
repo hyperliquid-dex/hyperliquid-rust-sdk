9a6775c5583792d7b76a107f385e87765c4921ec
tedison <timothyedison@hotmail.com>
2024-11-23 14:25:55 -0500

    make all crates public

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
 #![deny(unreachable_pub)]
-mod consts;
-mod errors;
-mod exchange;
-mod helpers;
-mod info;
-mod market_maker;
-mod meta;
-mod prelude;
-mod proxy_digest;
-mod req;
-mod signature;
-mod ws;
+pub mod consts;
+pub mod errors;
+pub mod exchange;
+pub mod helpers;
+pub mod info;
+pub mod market_maker;
+pub mod meta;
+pub mod prelude;
+pub mod proxy_digest;
+pub mod req;
+pub mod signature;
+pub mod ws;
 pub use consts::{EPSILON, LOCAL_API_URL, MAINNET_API_URL, TESTNET_API_URL};
 pub use errors::Error;
 pub use exchange::*;
