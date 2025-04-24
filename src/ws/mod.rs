b65f302edf10905eb9123d13e841c4213a044889
0xevolve <Artevolve@yahoo.com>
2025-04-24 18:47:07 +0100

    feat: post response types

--- src/ws/mod.rs
+++ src/ws/mod.rs
@@ -1,7 +1,13 @@
 mod message_types;
 mod sub_structs;
 mod ws_manager;
+mod responses;
+
 pub use message_types::*;
 pub use sub_structs::*;
+pub use responses::*;
 pub(crate) use ws_manager::WsManager;
 pub use ws_manager::{Message, Subscription};
+
+#[cfg(test)]
+mod tests;
