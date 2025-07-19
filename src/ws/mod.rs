b4ab3f5e41cc8c64ce1c141f3454d0004b98bdea
Austin <austinzhang1018@gmail.com>
2025-07-19 19:53:41 -0400

    added ws place and cancel methods

--- src/ws/mod.rs
+++ src/ws/mod.rs
@@ -1,7 +1,9 @@
 mod message_types;
 mod sub_structs;
 mod ws_manager;
+mod ws_post_client;
 pub use message_types::*;
 pub use sub_structs::*;
 pub(crate) use ws_manager::WsManager;
 pub use ws_manager::{Message, Subscription};
+pub use ws_post_client::WsPostClient;
