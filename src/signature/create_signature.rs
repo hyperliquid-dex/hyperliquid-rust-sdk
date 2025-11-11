2bc87324322725c978ed664ab02372be5c96000d
Luca Spinazzola <mspinluca@gmail.com>
2025-11-11 04:54:04 -0500

    revert debug print

--- src/signature/create_signature.rs
+++ src/signature/create_signature.rs
@@ -1,7 +1,6 @@
 use crate::exchange::MultiSigAction;
 use crate::{eip712::Eip712, prelude::*, signature::agent::l1, Actions, Error};
 use alloy::{
-    hex,
     primitives::B256,
     signers::{local::PrivateKeySigner, Signature, SignerSync},
 };
@@ -114,7 +113,6 @@ pub(crate) fn sign_multi_sig_action(
 
     let mut bytes = rmp_serde::to_vec_named(&action_without_type)
         .map_err(|e| Error::RmpParse(e.to_string()))?;
-    println!("{}", hex::encode(&bytes));
 
     bytes.extend(nonce.to_be_bytes());
 
