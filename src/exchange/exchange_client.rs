700679d3879d37fff366b3a6e3f150fe33f303b5
Luca Spinazzola <mspinluca@gmail.com>
2025-11-11 04:01:00 -0500

    Reapply "debug with hardcoded nonce"
    This reverts commit c660e12a5b57dfdb1eab7586db605fa4df8506f6.

--- src/exchange/exchange_client.rs
+++ src/exchange/exchange_client.rs
@@ -1186,7 +1186,7 @@ impl ExchangeClient {
             "Testnet".to_string()
         };
 
-        let timestamp = next_nonce();
+        let timestamp = 1762838147000;
 
         let send_asset = SendAsset {
             signature_chain_id: 421614,
